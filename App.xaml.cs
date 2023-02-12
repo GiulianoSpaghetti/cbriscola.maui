de73d5734c26cbbfddf723a7b111586f67f9d384
Giulio Sorrentino <gsorre84@gmail.com>
2023-02-12 07:14:29 +0100

    disab ilitata l'app shell per android se si compila su windows

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,7 +5,10 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-
+#if ANDROID
 		MainPage = new AppShell();
+#elif WINDOWS
+        MainPage=new AppShellWindows();
+#endif
 	}
 }
---
+++ AppShellWindows.xaml.cs
@@ -0,0 +1,9 @@
+namespace CBriscola2._0;
+
+public partial class AppShellWindows : Shell
+{
+    public AppShellWindows()
+    {
+        InitializeComponent();
+    }
+}
